9585552e64f56b4b1a3be7f88a35126057323abb
Vinnie Falco <vinnie.falco@gmail.com>
2023-04-19 08:44:09 -0700

    revert to mostly default antora ui

--- src/helpers/var.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-module.exports = (name, value, options) => {
-  if (typeof options.data.root !== 'object') {
-    options.data.root = Object()
-  }
-  options.data.root[name] = value
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
-  darkMode: 'class'
+  plugins: []
 }
